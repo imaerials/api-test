cdfd2a22e8adf7e981d9ea9876f3d50613b5d479
arielro85 <arielro85@gmail.com>
2020-06-11 22:58:16 -0300

    chequeo de password y autenticacion

--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const User = require('../models/User')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const appConfig = require('../config/appConfig')
+const User = require("../models/User");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const appConfig = require("../config/appConfig");
+const bcrypt = require("bcrypt");
 //index users
-router.get('/', (req, res) =>{
-  console.log('Index users')
+router.get("/", (req, res) => {
+  console.log("Index users");
   res.status(200).json({
-    msg: 'Estas en users'
-  })
-  
-
+    msg: "Estas en users",
+  });
 });
 //List users
-router.get('/list', async(req, res) =>{
-  console.log('listing users')
+router.get("/list", async (req, res) => {
+  console.log("listing users");
 
- const users= await User.find({},(err,users)=>{
+  const users = await User.find({}, (err, users) => {
     res.status(200).json({
-      users
-    })
- })
-  
-
+      users,
+    });
+  });
 });
 //create user
-router.post('/create',(req,res)=>{
-  console.log('creating users')
-     User.create({...req.body},(err,user)=>{
-      res.status(200).json({
-        user
-      })
-
-    }) 
-})
+router.post("/create", (req, res) => {
+  console.log("creating users");
+  User.create({ ...req.body }, (err, user) => {
+    res.status(200).json({
+      user,
+    });
+  });
+});
 //login user
-router.post('/login',async (req,res)=>{
-  const {email,password} = req.body
-  await User.findOne({email},(err,user)=>{
-    jwt.sign({ user}, appConfig.secret, (err, token)=> {
-      console.log(token);
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  await User.findOne({ email }, (err, user) => {
+    if (!user) {
+      return res.status(404).json({ error: "user not found" });
+    }
+    bcrypt.compare(password, user.password, function (err, match) {
+      if (match) {
+        jwt.sign({ user }, appConfig.secret, (err, token) => {
+          console.log(token);
+        });
+
+        return res.status(200).json({
+          msg: "Congratss",
+          user: user.name,
+        });
+      } else {
+        return res.status(404).json({
+          msg: "incorrect password",
+        });
+      }
     });
 
-    console.log('Login user found: ',user)
-  })
- // const user = User.
-  res.status(200).json({
-    msg: 'ok user'
-  })
-})
-
-
+    console.log(user.name);
+  });
+});
 
 module.exports = router;
